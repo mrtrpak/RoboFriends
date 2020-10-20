3dd8bf958eb29b935af0f6ae12204dca9d914e98
Michael Trpak <mrtrpak3@gmail.com>
2020-10-20 16:03:48 -0400

    adding list of robots

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Katja Swift",
+    email: "theSwiftness@email.com"
+  },
+  {
+    id: 2,
+    name: "Manu Schopf",
+    email: "manu@email.com"
+  },
+  {
+    id: 3,
+    name: "Egor Schivkov",
+    email: "getTheShiv@email.com"
+  },
+  {
+    id: 4,
+    name: "Bob Johnson",
+    email: "plain@email.com"
+  },
+  {
+    id: 5,
+    name: "Alejandro Suarez",
+    email: "AleSuarez@email.com"
+  },
+  {
+    id: 6,
+    name: "Clementine Spring",
+    email: "springClem@email.com"
+  }
+]
