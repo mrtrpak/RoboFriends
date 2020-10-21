d56be2449d54a32192560117a6580f93e13b31e9
Michael Trpak <mrtrpak3@gmail.com>
2020-10-21 14:59:28 -0400

    importing in search box and adding title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 export default function App() {
   return (
-    <CardList robots={robots} />
+    <div>
+      <h1>RoboFriends</h1>
+      <SearchBox />
+      <CardList robots={robots} />
+    </div>
   );
 };
