2394c4e5267fbe9eeff8a27d7a13c63186fda977
Michael Trpak <mrtrpak3@gmail.com>
2020-10-21 15:13:44 -0400

    adding in search input

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function SearchBox() {
   return (
-    <div>
-      
+    <div className='pa2'>
+      <input className='pa3 ba b--green bg blue' type='search' placeholder='search robots' />
     </div>
-  )
-}
+  );
+};
