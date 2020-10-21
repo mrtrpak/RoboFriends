6763bfa65d8ade13ecda4d531ec839e0c875a6c7
Michael Trpak <mrtrpak3@gmail.com>
2020-10-21 14:55:33 -0400

    importing in App and shortening code

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import CardList from './CardList';
+import App from './App';
 import './index.css';
 import 'tachyons';
-import { robots } from "./robots";
 
-ReactDOM.render(
-  <div>
-    <CardList robots={robots} />
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// If you want your app to work offline and load faster, change unregister() to register() below. 
 serviceWorker.unregister();
