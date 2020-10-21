919d879ac2ce2f6651f33c83d7f37a8c3fc7be18
Michael Trpak <mrtrpak3@gmail.com>
2020-10-21 14:32:29 -0400

    adding spacing

--- src/CardList.js
+++ src/CardList.js
@@ -3,9 +3,10 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  
+
   const cardComponent = robots.map((user, i) => {
     const robot = robots[i];
+    
     return (
       <Card key={i} id={robot.id} name={robot.name} email={robot.email} />
     );
