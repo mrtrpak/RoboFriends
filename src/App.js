84d8a984b832377bc4b817a42b6144e4646a7795
Michael Trpak <mrtrpak3@gmail.com>
2020-10-22 13:28:44 -0400

    changing app to react component to be able to use state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-export default function App() {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
+const state = {
+  robots: robots,
+  searchfield: ""
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    );
+  }
 };
+
+export default App;
